2ffe2e0b16980fc0b290745a6c2de9683e17ab26
Pushp Singh <pushp.singh@SW-LP02545.local>
2019-08-09 10:20:03 +0530

    added docs folder with index file

---
+++ docs/precache-manifest.2069a51c9bc239a92e388caa82b24d4f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcd582873523ba2d9f47f2ce20160247",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a417d94840f5853576eb",
+    "url": "/static/css/2.8260bb11.chunk.css"
+  },
+  {
+    "revision": "4fac766245b957776924",
+    "url": "/static/css/main.c023c813.chunk.css"
+  },
+  {
+    "revision": "a417d94840f5853576eb",
+    "url": "/static/js/2.f88fe2e3.chunk.js"
+  },
+  {
+    "revision": "4fac766245b957776924",
+    "url": "/static/js/main.0cea00aa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5c70e70d838c0869ba4cb4823abfc68c",
+    "url": "/static/media/anatta-logo.5c70e70d.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
